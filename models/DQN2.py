370925157241a017a74e1d7ccb4b3d1d31bd032a
David Han <dmh338@cornell.edu>
2022-06-22 22:44:15 -0400

    Add models

---
+++ models/DQN2.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+class DQN2(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        #this could be one block but idc 
+        self.l1 = nn.Sequential(nn.Linear(1,69), nn.ReLU()) #add whatever hidden layers ig
+        self.l2 = nn.Sequential(nn.Linear(69, 420), nn.ReLU())
+        self.l3 = nn.Sequential(nn.Linear(420, 1))
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = self.l2(x)
+        x = self.l3(x)
+
+        return x
